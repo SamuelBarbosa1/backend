82cca3d958122f85ff718d7e6d433950d9f8c605
SamuelBarbosa1 <samuelbarbosa2001oliveira@gmail.com>
2024-09-05 15:32:32 -0300

    add

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"github.com/SamuelBarbosa1/ticket-booking-project-v1/config"
+	"github.com/SamuelBarbosa1/ticket-booking-project-v1/db"
 	"github.com/SamuelBarbosa1/ticket-booking-project-v1/handlers"
 	"github.com/SamuelBarbosa1/ticket-booking-project-v1/repositories"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	envConfig := config.NewEnvConfig()
+	db := db.Init(envConfig, db.DBMigrator)
+
 	app := fiber.New(fiber.Config{
 		AppName:      "TicketBooking",
 		ServerHeader: "Fiber",
 	})
 
 	// Repositories
-	EventRepository := repositories.NewEventRepository(nil)
+	EventRepository := repositories.NewEventRepository(db)
 
 	// Routing
 	server := app.Group("/api")
